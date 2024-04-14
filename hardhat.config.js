687db1ae5c5a1221a7acafd3e4ab209f4e2538cb
Karthik S <karthiks0598@gmail.com>
2024-04-14 17:42:04 +0530

    feat(deploy): implement deploy scripts for  mock & raffle .sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,41 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const PRIVATE_KEY =
+    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            // // If you want to do some forking, uncomment this
+            // forking: {
+            //   url: MAINNET_RPC_URL
+            // }
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+    },
+    sepolia: {
+        url: SEPOLIA_RPC_URL,
+        accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+        chainId: 11155111,
+        blockConfirmation: 6,
+    },
     solidity: "0.8.7",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
